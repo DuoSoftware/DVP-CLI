7ec59f13677bdbcdba342566193f5ba3edeccb70
sukitha <sukithaj@gmail.com>
2015-05-21 17:29:50 +0530

    basic installer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/fsouza/go-dockerclient"
+	"github.com/jmcvetta/restclient"
 	"os"
 )
 
@@ -116,6 +117,208 @@ func main() {
 				} else {
 
 					fmt.Printf("Image not found ----------------> %s", name)
+
+				}
+
+			},
+		},
+
+		{
+
+			Name:  "install",
+			Usage: "install templates in given host",
+
+			Flags: []cli.Flag{
+
+				cli.StringFlag{
+					Name:  "protocol",
+					Value: "http",
+					Usage: "docker remote api protocol to connect",
+				},
+				cli.StringFlag{
+					Name:  "host",
+					Value: "127.0.0.1",
+					Usage: "docker host ip",
+				},
+				cli.StringFlag{
+					Name:  "port",
+					Value: "4243",
+					Usage: "docker host port",
+				},
+				cli.StringFlag{
+					Name:  "unixsocket",
+					Value: "var/run/docker.sock",
+					Usage: "docker unix socket path",
+				},
+				cli.StringFlag{
+					Name:  "template",
+					Value: "DBTEMPLATE",
+					Usage: "template name to be install",
+				},
+			},
+
+			Action: func(c *cli.Context) {
+
+				protocol := c.String("protocol")
+				host := c.String("host")
+				port := c.String("port")
+				socket := c.String("unixsocket")
+				template := c.String("template")
+
+				fmt.Printf("Image ----------------> %s", c)
+
+				endpoint := fmt.Sprintf("http://%s:%s", host, port)
+
+				if protocol == "unix" {
+
+					endpoint = fmt.Sprintf("unix:///%s", socket)
+
+				}
+
+				client, _ := docker.NewClient(endpoint)
+				//client.ListContainers(docker.ListContainersOptions{All: false})
+
+				/////////--------------------------------------------------------------------------------------------->
+
+				type Variable struct {
+					Name         string
+					Description  string
+					DefaultValue string
+					Export       bool
+					Type         string
+				}
+
+				type TemplateImage struct {
+					Type string
+				}
+
+				type Image struct {
+					Name               string
+					Description        string
+					Version            string
+					VersionStatus      string
+					SourceUrl          string
+					DockerUrl          string
+					Class              string
+					Type               string
+					Cmd                string
+					Category           string
+					Importance         string
+					CSDB_TemplateImage TemplateImage
+					SystemVariables    []Variable
+					Dependants         []Image
+				}
+
+				type Template struct {
+					Name          string
+					Description   string
+					Class         string
+					Type          string
+					Category      string
+					CompanyId     int
+					TenantId      int
+					TemplateImage []Image
+				}
+
+				type Result struct {
+					Exception     string
+					CustomMessage string
+					IsSuccess     bool
+					Result        []Template
+				}
+
+				url := fmt.Sprintf("http://127.0.0.1:9093/DVP/API/1.0/SystemRegistry/TemplateByName/%s", template)
+
+				//"http://127.0.0.1:9093/DVP/API/1.0/SystemRegistry/TemplateByName/DBTEMPLATE"
+
+				s := Result{}
+
+				r := restclient.RequestResponse{
+					Url:    url,
+					Method: "GET",
+					Result: &s,
+				}
+				status, err := restclient.Do(&r)
+				if err != nil {
+					//panic(err)
+				}
+				if status == 200 {
+					fmt.Println("Template Data  -->", s.Result)
+
+					if s.IsSuccess == true {
+
+						if s.Result != nil {
+
+							for _, temp := range s.Result {
+
+								for _, img := range temp.TemplateImage {
+									fmt.Println(img.CSDB_TemplateImage.Type)
+									if img.CSDB_TemplateImage.Type == "Mandetory" {
+
+										fmt.Println(img.Class)
+										if img.Class == "DOCKER" {
+
+											fmt.Println(img.DockerUrl)
+
+											pullImage := docker.PullImageOptions{Repository: img.DockerUrl, Tag: "latest"}
+											authConf := docker.AuthConfiguration{}
+											erry := client.PullImage(pullImage, authConf)
+											fmt.Printf("pull --->", erry)
+											if erry == nil {
+
+												container := docker.CreateContainerOptions{}
+												container.Name = img.Name
+												img.Cmd = "postgres"
+												cmd := []string{img.Cmd}
+
+												/*
+													a := []int{1,2,3}
+													a = append(a, 4)
+													fmt.Println(a)
+
+												*/
+
+												Var := []string{}
+
+												for _, vars := range img.SystemVariables {
+
+													Var = append(Var, fmt.Sprintf("%s=%s", vars.Name, vars.DefaultValue))
+
+												}
+
+												container.Config = &docker.Config{Image: img.DockerUrl, Cmd: cmd, Env: Var}
+
+												fmt.Println(container.Config.Image)
+
+												_, errx := client.CreateContainer(container)
+
+												fmt.Printf("Container --->", errx)
+
+												if errx == nil {
+
+													//fmt.Printf("Container ---> ", cont)
+
+													hostConfig := &docker.HostConfig{}
+
+													errz := client.StartContainer(img.Name, hostConfig)
+													fmt.Printf("Container --->", errz)
+												}
+											}
+
+										} else if img.Type == "DOCKERFILE" {
+
+										}
+
+									} else if img.CSDB_TemplateImage.Type == "Optional" {
+
+									}
+
+								}
+
+							}
+
+						}
+					}
 
 				}
 
